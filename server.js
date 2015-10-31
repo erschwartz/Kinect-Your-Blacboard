488ddc4ddf1ba9ff57e738364be5b85dadcd3170
bakpakin <calsrose@gmail.com>
2015-10-31 17:48:01 -0400

    Remove mongo, readd gitignore.

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 
 // Require relevent stuff
-var MongoClient = require('mongodb').MongoClient;
-var ObjectId = require('mongodb').ObjectID;
 var assert = require('assert');
 var http = require('http');
 var express = require('express');
 var app = express();
 
-var mongoUrl = 'mongodb://localhost:27017/myproject';
 var dataBase;
 
 app.set('port', (process.env.PORT || 5000));
@@ -23,22 +20,4 @@ app.get('/', function(request, response) {
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
-});
-
-// Helper functions
-var makeSession = function(name, description) {
-
-    dataBase.collection("sessions").insertOne( {
-        "name": name,
-        "description": "A new session.",
-        "listeners": []
-    });
-    console.log("Inserted a document into the sessions collection.");
-    return true;
-}
-
-MongoClient.connect(mongoUrl, function(err, db) {
-    assert.equal(null, err);
-    console.log("Connected correctly to server.");
-    dataBase = db;
 });
