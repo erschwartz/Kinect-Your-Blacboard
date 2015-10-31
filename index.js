d66b2890c6637c60fe1821a5ff51676c5f68893b
Erika Schwartz <erikaschwartz@comcast.net>
2015-10-31 17:13:06 -0400

    Express

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-// This one node.js instance manages both the mongodb and the requests from the client.
 
 // Require relevent stuff
 var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectID;
 var assert = require('assert');
 var http = require('http');
+var express = require('express');
+var app = express();
 
 var mongoUrl = 'mongodb://localhost:27017/myproject';
 var dataBase;
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
 
 // Helper functions
 var makeSession = function(name, description) {
@@ -32,8 +49,3 @@ function handleRequest(request, response) {
     response.end("It works." + request.url);
 }
 
-var server = http.createServer(handleRequest);
-
-server.listen(PORT, function() {
-    console.log("Server listening on: http://localhost:" + PORT);
-});
