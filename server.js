4f5f005bbde7e66ee9ee0c34071172bdee3b417e
bakpakin <calsrose@gmail.com>
2015-11-01 04:26:20 -0500

    hjdkjas

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ var server = app.listen(app.get('port'), function() {
   var port = app.get('port')
   console.log('Node app is running at http://localhost:%s', port);
 });
-//
+
 renderPaths("out.png", [[
   {x:0,y:0},
   {x:100,y:100}
